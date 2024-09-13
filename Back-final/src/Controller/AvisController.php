72cf84d57fcaaa5e73d2c7bbbafbcf4ca6e42f36
chloebaisse1 <baissechloe@gmail.com>
2024-09-13 08:56:28 +0400

    integration serializer controller Avis

--- Back-final/src/Controller/AvisController.php
+++ Back-final/src/Controller/AvisController.php
@@ -6,67 +6,71 @@ use App\Entity\Avis;
 use App\Repository\AvisRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('api/avis', name: 'app_api_avis_')]
 class AvisController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $manager, private AvisRepository $repository)
+    public function __construct(
+    private EntityManagerInterface $manager,
+    private AvisRepository $repository,
+    private SerializerInterface $serializer,
+    private UrlGeneratorInterface $urlGenerator,
+    )
     {
     }
 
 
-    #[Route(name: 'new', methods: 'POST')]
-    public function new(): Response
+    #[Route( methods: 'POST')]
+    public function new(Request $request): JsonResponse
     {
-        $avis = new Avis();
-        $avis->setNom('John');
-        $avis->setPrenom('Doe');
-        $avis->setMessage('Le zoo est magnifique');
-
+        $avis = $this->serializer->deserialize($request->getContent(), Avis::class, 'json');
+        $avis->setCreatedAt(new \DateTimeImmutable());
 
         $this->manager->persist($avis);
         $this->manager->flush();
 
-        // a stocker en base de données
-        return $this->json(
-            ['message' => 'Avis créé'],
-            Response::HTTP_CREATED
+        $responseData = $this->serializer->serialize($avis, 'json');
+        $location = $this->urlGenerator->generate(
+        'app_api_avis_show',
+        ['id' => $avis->getId()],
+        UrlGeneratorInterface::ABSOLUTE_URL
         );
+
+        return new JsonResponse($responseData, Response::HTTP_CREATED, ["Location"=> $location], true);
     }
+
 
     #[Route('/{id}', name: 'show', methods: 'GET')]
-    public function show(int $id): Response
+    public function show(int $id): JsonResponse
     {
         $avis = $this->repository->findOneBy(['id' => $id]);
+        if($avis){
+            $responseData = $this->serializer->serialize($avis, 'json');
 
-            if(!$avis){
-                throw new BadRequestException('Avis non trouvé pour {id} id');
-            }
-            return $this->json(
-                ['message'=> "Avis trouvé : {$avis->getNom()} pour {$avis->getId()} id"]
-            );
-    }
+        return new JsonResponse($responseData, Response::HTTP_OK, [], true);
+        }
 
-
-
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+}
 
 
 
     #[Route('/{id}', name: 'delete', methods: 'DELETE')]
-    public function delete(int $id): Response
+    public function delete(int $id): JsonResponse
     {
         $avis = $this->repository->findOneBy(['id' => $id]);
-
-        if(!$avis){
-
-            throw new BadRequestException('Avis non trouvé pour {id} id');
-        }
-
+        if($avis){
         $this->manager->remove($avis);
         $this->manager->flush();
-        return $this->json(['message' => 'Avis supprimé'], Response::HTTP_NO_CONTENT);
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 }
