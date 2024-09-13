f9c0ae67d8095e25d3bfe461d01f6d28eaa55846
chloebaisse1 <baissechloe@gmail.com>
2024-09-13 11:39:21 +0400

    ajout de la route account/me

--- Back-final/src/Controller/SecurityController.php
+++ Back-final/src/Controller/SecurityController.php
@@ -11,12 +11,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api', name: 'app_api_')]
 class SecurityController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $manager, private SerializerInterface $serializer)
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private SerializerInterface $serializer,
+        private UserPasswordHasherInterface $passwordHasher,
+        )
     {
     }
 
@@ -50,6 +55,38 @@ class SecurityController extends AbstractController
             'apiToken' => $user->getApiToken(),
             'roles' => $user->getRoles()
         ]);
+    }
+
+    #[Route('/account/me', name: 'me', methods: 'GET')]
+    public function me(): JsonResponse
+    {
+        $user = $this->getUser();
+
+        $responseData = $this->serializer->serialize($user, 'json');
+
+        return new JsonResponse($responseData, Response::HTTP_OK, [], true);
+    }
+
+
+    #[Route('/account/edit', name: 'edit', methods: 'PUT')]
+
+    public function edit(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
+        $user = $this->serializer->deserialize(
+            $request->getContent(),
+            User::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $this->getUser()],
+        );
+        $user->setUpdatedAt(new \DateTimeImmutable());
+
+        if (isset($request->toArray()['password'])) {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+        }
+
+        $this->manager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
 
