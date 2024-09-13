873eed71fc1553bba9308387ad9c6d92e22b48dd
chloebaisse1 <baissechloe@gmail.com>
2024-09-13 15:16:19 +0400

    Documentation API Nelmio CRUD Animal

--- Back-final/src/Controller/AnimalController.php
+++ Back-final/src/Controller/AnimalController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Animal;
 use App\Repository\AnimalRepository;
+use OpenApi\Attributes as OA;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -28,6 +29,42 @@ class AnimalController extends AbstractController
 
 
     #[Route(name: 'new', methods: 'POST')]
+    #[OA\Post(
+        path: "/api/animal",
+        summary: "Creation d'un animal",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "Données de l'animal à creer",
+            content: new OA\JsonContent(
+                type: "object",
+                properties: [
+                    new OA\Property(property: "nom", type: "string", example: "nom de l'animal"),
+                    new OA\Property(property: "race", type: "string", example: "race de l'animal"),
+                    new OA\Property(property: "habitat", type: "string", example: "habitat de l'animal"),
+                    new OA\Property(property: "etat", type: "string", example: "etat de santé de l'animal"),
+
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: "Animal crée avec succès",
+                content: new OA\JsonContent(
+                    type: "object",
+                    properties: [
+                        new OA\Property(property: "id", type: "integer", example: 1),
+                        new OA\Property(property: "nom", type: "string", example: "Nom de l'animal"),
+                        new OA\Property(property: "race", type: "string", example: "Race de l'animal"),
+                        new OA\Property(property: "habitat", type: "string", example: "habitat de l'animal"),
+                        new OA\Property(property: "etat", type: "string", example: "etat de santé de l'animal"),
+                        new OA\Property(property: "createdAt", type: "string", format: "date-time"),
+                    ]
+                )
+            )
+        ]
+    )]
+
     public function new(Request $request): JsonResponse
     {
         $animal = $this->serializer->deserialize($request->getContent(), Animal::class, 'json');
@@ -47,6 +84,41 @@ class AnimalController extends AbstractController
     }
 
     #[Route('/{id}', name: 'show', methods: 'GET')]
+    #[OA\Get(
+        path: "/api/animal/{id}",
+        summary: "Afficher un animal par son ID",
+        parameters: [
+            new OA\Parameter(
+                name: "id",
+                in: "path",
+                required: true,
+                description: "ID de l'animal à afficher",
+                schema: new OA\Schema(type: "integer")
+            )
+        ],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Animal trouvé avec succès",
+                content: new OA\JsonContent(
+                    type: "object",
+                    properties: [
+                        new OA\Property(property: "id", type: "integer", example: 1),
+                        new OA\Property(property: "nom", type: "string", example: "Nom de l'animal"),
+                        new OA\Property(property: "race", type: "string", example: "race de l'animal"),
+                        new OA\Property(property: "habitat", type: "string", example: "habitat de l'animal"),
+                        new OA\Property(property: "etat", type: "string", example: "etat de santé de l'animal"),
+                        new OA\Property(property: "createdAt", type: "string", format: "date-time")
+                    ]
+                )
+            ),
+            new OA\Response(
+                response: 404,
+                description: "Animal non trouvé"
+            )
+        ]
+    )]
+
     public function show(int $id): JsonResponse
     {
         $animal = $this->repository->findOneBy(['id' => $id]);
@@ -62,6 +134,42 @@ class AnimalController extends AbstractController
 
 
     #[Route('/{id}', name: 'edit', methods: 'PUT')]
+    #[OA\Put(
+        path: "/api/animal/{id}",
+        summary: "Modifier un animal par ID",
+        parameters: [
+            new OA\Parameter(
+                name: "id",
+                in: "path",
+                required: true,
+                description: "ID de l'animal à modifier",
+                schema: new OA\Schema(type: "integer")
+            )
+        ],
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "Nouvelles données de l'animal à mettre à jour",
+            content: new OA\JsonContent(
+                type: "object",
+                properties: [
+                    new OA\Property(property: "nom", type: "string", example: "Nouveau nom de l'animal"),
+                    new OA\Property(property: "race", type: "string", example: "Nouvelle race de l'animal"),
+                    new OA\Property(property: "habitat", type: "string", example: "Nouveau habitat de l'animal"),
+                    new OA\Property(property: "etat", type: "string", example: "Nouveau etat de santé de l'animal"),
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 204,
+                description: "Animal modifié avec succès"
+            ),
+            new OA\Response(
+                response: 404,
+                description: "Animal non trouvé"
+            )
+        ]
+    )]
     public function edit(int $id, Request $request): JsonResponse
     {
         $animal = $this->repository->findOneBy(['id' => $id]);
@@ -85,6 +193,29 @@ class AnimalController extends AbstractController
 
 
     #[Route('/{id}', name: 'delete', methods: 'DELETE')]
+    #[OA\Delete(
+        path: "/api/animal/{id}",
+        summary: "Supprimer un animal par son ID",
+        parameters: [
+            new OA\Parameter(
+                name: "id",
+                in: "path",
+                required: true,
+                description: "ID de l'animal à supprimer",
+                schema: new OA\Schema(type: "integer")
+            )
+        ],
+        responses: [
+            new OA\Response(
+                response: 204,
+                description: "Animal supprimé avec succès",
+            ),
+            new OA\Response(
+                response: 404,
+                description: "Animal non trouvé"
+            )
+        ]
+    )]
     public function delete(int $id): JsonResponse
     {
         $animal = $this->repository->findOneBy(['id' => $id]);
