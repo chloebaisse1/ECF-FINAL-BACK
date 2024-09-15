c46b7e42c90388ac8aafecbbfa399bd8f08de462
chloebaisse1 <baissechloe@gmail.com>
2024-09-15 10:53:26 +0400

    page habitat savane

--- front-final/Router/allRoutes.js
+++ front-final/Router/allRoutes.js
@@ -1,7 +1,14 @@
 import Route from "./Route.js"
 
 //Définir ici vos routes
-export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")]
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/jungle", "Jungle", "/pages/jungle.html"),
+  new Route("/savane", "Savane", "/pages/savane.html"),
+  new Route("/marais", "Marais", "/pages/marais.html"),
+  new Route("/contact", "Contact", "/pages/contact.html"),
+  new Route("/avis", "Avis", "/pages/avis.html"),
+]
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia"
