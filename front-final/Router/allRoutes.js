a94ec33d06a0d33ac8cb4f4dfedd3f06b0460ba5
chloebaisse1 <baissechloe@gmail.com>
2024-09-16 08:09:28 +0400

    rangement des pages dans un dossier + creation de la page Mon compte

--- front-final/Router/allRoutes.js
+++ front-final/Router/allRoutes.js
@@ -8,8 +8,9 @@ export const allRoutes = [
   new Route("/marais", "Marais", "/pages/habitats/marais.html"),
   new Route("/contact", "Contact", "/pages/contact.html"),
   new Route("/avis", "Avis", "/pages/avis.html"),
-  new Route("/signin", "Connexion", "/pages/signin.html"),
-  new Route("/signup", "Inscription", "/pages/signup.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route("/account", "Mon compte", "pages/auth/account.html"),
 ]
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
